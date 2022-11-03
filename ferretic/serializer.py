23d3f914918601cd44b7cfe0b9142af66dcac615
carlosceballos0427 <santos.programando@gmail.com>
2022-11-03 15:14:19 -0500

    ferretic Con seguridad

--- ferretic/serializer.py
+++ ferretic/serializer.py
@@ -1,20 +1,24 @@
 from rest_framework import serializers
 from ferretic.models import *
 
+
 class Usuario_serializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = "__all__"
-def create(self,validated_data):
-    user = Usuario(
-        username=validated_data["username"],
-        nombres_apellidos=validated_data["nombres_apellidos"],
-        correo=validated_data["correo"],
-        telefono=validated_data["telefono"],
-    )
-    user.set_password(validated_data["password"])
-    user.save()
-    return user
+
+
+    def create(self,validated_data):
+        user = Usuario(
+            username=validated_data["username"],
+            nombres_apellidos=validated_data["nombres_apellidos"],
+            correo=validated_data["correo"],
+            telefono=validated_data["telefono"],
+
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
 
 
 class Producto_serializer(serializers.ModelSerializer):
@@ -22,28 +26,32 @@ class Producto_serializer(serializers.ModelSerializer):
         model = Producto
         fields = "__all__"
 
+
 class Proveedor_serializer(serializers.ModelSerializer):
     class Meta:
         model = Proveedor
         fields = "__all__"
+
 
 class Cliente_serializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = "__all__"
 
+
 class Empleado_serializer(serializers.ModelSerializer):
     class Meta:
         model = Empleado
         fields = "__all__"
 
+
 class Venta_serializer(serializers.ModelSerializer):
     producto = Producto_serializer(read_only=True)
-    producto_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Producto.objects.all(), source="producto")
+    producto_id = serializers.PrimaryKeyRelatedField(write_only=True,queryset=Producto.objects.all(), source="producto")
     cliente = Cliente_serializer(read_only=True)
     cliente_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Cliente.objects.all(), source="cliente")
     empleado = Empleado_serializer(read_only=True)
-    empleado_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Empleado.objects.all(), source="empleado")
+    empleado_id = serializers.PrimaryKeyRelatedField(write_only=True,queryset=Empleado.objects.all(), source="empleado")
     class Meta:
         model = Venta
         fields = "__all__"
@@ -55,16 +63,20 @@ class Compras_serializer(serializers.ModelSerializer):
     proveedor = Proveedor_serializer(read_only=True)
     proveedor_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Proveedor.objects.all(), source="proveedor")
     empleado = Empleado_serializer(read_only=True)
-    empleado_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Empleado.objects.all(), source="empleado")
+    empleado_id = serializers.PrimaryKeyRelatedField(write_only=True,queryset=Empleado.objects.all(), source="empleado")
+
+
     class Meta:
         model = Compras
         fields = "__all__"
+
 
 class Almacen_serializer(serializers.ModelSerializer):
     cantidad_compra = Producto_serializer(read_only=True)
     cantidad_compra_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Compras.objects.all(), source="cantidad_compra")
     cantidad_venta = Proveedor_serializer(read_only=True)
     cantidad_venta_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Venta.objects.all(), source="cantidad_venta")
+
     class Meta:
         model = Almacen
         fields = "__all__"
